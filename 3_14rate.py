509c450a1aefde09094e0b8be5920595badc153e
Oleg Yakovlev <yablinchik@gmail.com>
2020-03-18 22:00:56 +0500

    f*ck

--- 3_14rate.py
+++ 3_14rate.py
@@ -12,17 +12,17 @@ def log(msg):
         sys.stderr.write(str(msg)+"\n")
         sys.stderr.flush()
 
-log(data,file=sys.stderr)
+log(data)
 if data["tor_status"] == 2:
     val = math.floor(int(data["seeders"])*2**40//int(data["size"])*24*60*60//(time.time()-int(data["seeder_last_seen"])))
     if KP:
         try:
-            log("KP lib found",file=sys.stderr)
+            log("KP lib found")
             from kinopoisk.movie import Movie
             movie = Movie.objects.search( data["topic_title"].split("/")[0])[0]
-            log(movie.title,file=sys.stderr)
+            log(movie.title)
             r= (movie.rating if not movie.rating is None else 0)
-            log(r,file=sys.stderr)
+            log(r)
             val= math.floor((r+1)*val)
         except ImportError:
             pass
