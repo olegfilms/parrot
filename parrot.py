cf511b4a9960afee1d1043066db62adb6a3df50a
yablinchik <yablinchik@gmail.com>
2020-03-19 12:24:01 +0000

    We only support python 3

--- parrot.py
+++ parrot.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 # -*- coding: utf-8 -*-
 import json,gzip,os,urllib.request,re,urllib.parse,urllib.error
 import pathlib,configparser,sqlite3,argparse,time,subprocess
